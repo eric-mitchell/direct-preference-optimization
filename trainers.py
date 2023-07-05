7dd1236e654cbea2f72037fde8e5164c7d515f54
Eric Mitchell <em7@stanford.edu>
2023-07-05 15:17:48 -0700

    Added config attribute to only generate model samples for a subset of prompts.

--- trainers.py
+++ trainers.py
@@ -276,7 +276,7 @@ class BasicTrainer(object):
                     if self.config.loss.name == 'dpo':
                         reference_text_table = wandb.Table(columns=["step", "prompt", "sample"])
 
-                for eval_batch in (tqdm.tqdm(self.eval_batches) if self.rank == 0 else self.eval_batches):
+                for eval_batch in (tqdm.tqdm(self.eval_batches, desc='Computing eval metrics') if self.rank == 0 else self.eval_batches):
                     local_eval_batch = slice_and_move_batch_for_device(eval_batch, self.rank, self.world_size, self.rank)
                     with torch.no_grad():
                         _, eval_metrics = self.get_batch_metrics(local_eval_batch, self.config.loss, train=False)
@@ -284,7 +284,11 @@ class BasicTrainer(object):
                     for k, v in eval_metrics.items():
                         all_eval_metrics[k].extend(v)
 
-                    if self.config.sample_during_eval:
+                if self.config.sample_during_eval:
+                    n_sample_batches = self.config.n_eval_model_samples // self.config.eval_batch_size
+                    sample_batches = self.eval_batches[:n_sample_batches]
+                    for eval_batch in (tqdm.tqdm(sample_batches, desc='Generating samples...') if self.rank == 0 else sample_batches):
+                        local_eval_batch = slice_and_move_batch_for_device(eval_batch, self.rank, self.world_size, self.rank)
                         if 'FSDP' in self.config.trainer:
                             with FSDP.summon_full_params(self.policy, writeback=False, recurse=False):
                                 policy_samples, reference_samples = self.get_batch_samples(local_eval_batch)
